a441d72e387765ad3e9801418fb8c543ec1610cf
肖寒 <xxxiaocat@gmail.com>
2021-03-11 18:03:02 +0800

    芜湖测试

---
+++ src/cn/easybuy/utils/DataSourceUtil.java
@@ -0,0 +1,62 @@
+package cn.easybuy.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+public class DataSourceUtil {
+	private static DataSource dataSource;
+	
+	private static String driver;
+	private static String url;
+	private static String user;
+	private static String password;
+	
+	static {
+		init();
+	}
+	
+	public static void init(){
+		Properties params=new Properties();
+		String configFile = "database.properties";
+		System.out.println(configFile);
+		InputStream is=DataSourceUtil.class.getClassLoader().getResourceAsStream(configFile);
+		System.out.println(is);
+		try {
+			params.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		driver=params.getProperty("driver");
+		url=params.getProperty("url");
+		user=params.getProperty("username");
+		password=params.getProperty("password");
+	}   
+
+	//获取连接
+	public static Connection openConnection() throws SQLException {
+		Connection connection = null;
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return connection;
+	}
+	//关闭连接
+	public static void closeConnection(Connection connection) {
+		try {
+			if (connection != null)
+				connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
